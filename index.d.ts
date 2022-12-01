eb55a80eaf055777e9ef12c3c94d7be9b13c67df
Don <don@phantom.app>
2022-12-01 15:05:10 -0500

    unbump version for cursor, add path for getting multichain blocklist

--- index.d.ts
+++ index.d.ts
@@ -5,15 +5,6 @@ export interface PhantomBlocklist {
   whitelist: string[]
 };
 
-export interface PhantomBlocklistMultichain {
-  contentHash: string,
-  blocklist: string[],
-  fuzzylist: string[],
-  whitelist: string[],
-  ethBlocklist: string[]
-};
-
-
 export declare function getVersion(): string;
 export declare function getBlocklist(): PhantomBlocklist;
-export declare function getBlocklistMultichain(): PhantomBlocklistMultichain;
+export declare function getBlocklistMultichain(): PhantomBlocklist;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { version } = require("./package.json");
 const blocklist = require("./blocklist.json");
+const blocklistMultichain = require("./multichain/blocklist.json");
 
 const getVersion = () => {
   return version;
@@ -9,5 +10,10 @@ const getBlocklist = () => {
   return blocklist;
 }
 
+const getBlocklistMultichain = () => {
+  return blocklistMultichain;
+}
+
 exports.getVersion = getVersion;
 exports.getBlocklist = getBlocklist;
+exports.getBlocklistMultichain = getBlocklistMultichain;
