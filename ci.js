995c4ac2defa36a47993422f5a2de3673583ee94
Don <don@phantom.app>
2022-12-01 11:29:43 -0500

    add build for multichain blocklist

--- ci.js
+++ ci.js
@@ -5,8 +5,14 @@ const blocklist = yaml.load(fs.readFileSync('./blocklist.yaml', 'utf8'));
 const nftBlocklist = yaml.load(fs.readFileSync('./nft-blocklist.yaml', 'utf8'));
 const whitelist = yaml.load(fs.readFileSync('./whitelist.yaml', 'utf8'));
 const fuzzylist = yaml.load(fs.readFileSync('./fuzzylist.yaml', 'utf8'));
+const ethBlocklist = yaml.load(fs.readFileSync('./eth-blocklist.yaml', 'utf8'));
 
 if(blocklist.some(item => !item.url)) {
+  console.log("Not every blocklist item has a `url` attribute");
+  process.exit(1);
+}
+
+if(ethBlocklist.some(item => !item.url)) {
   console.log("Not every blocklist item has a `url` attribute");
   process.exit(1);
 }
